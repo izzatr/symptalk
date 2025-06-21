6639049733902ac61ae62e89999411f2ac7f1b4d
izzatr <m.izzatraihan@gmail.com>
2025-06-21 07:50:20 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
+import { fileURLToPath } from 'url'
+import { dirname, resolve }   from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
+    config.resolve.alias['@'] = resolve(__dirname, 'src')
+    return config
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
